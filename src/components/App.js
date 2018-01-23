81da9c1f8918d27ef21736430e2ca4f6a1ca80cc
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2018-01-22 21:32:15 -0800

    pass a prop from App to Header

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
 		return (
 			<div className="catch-of-the-day">
 				<div className="menu">
-					<Header />
+					<Header cool={true} age="5000" tagline="Fresh Seafood Market"/>
 				</div>
 				<Order />
 				<Inventory />
