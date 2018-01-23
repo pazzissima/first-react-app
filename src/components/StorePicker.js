4624e06a4b1b79ccd2095ab06ef91dbe91984381
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2018-01-22 20:30:30 -0800

    created an empty form and button

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,7 +2,15 @@ import React from 'react';
 
 class StorePicker extends React.Component {
 	render() {
-		return <p>Hello</p>
+		// This type of comment here
+		return (
+			<form className="store-selector">
+				{/* This type of comment here */}
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store -></button>
+			</form>
+		)
 	}
 }
 
