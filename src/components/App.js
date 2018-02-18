07a880c4c4fbe4eaf5586bfd494c5208ff54d539
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2018-02-18 12:39:50 -0800

    add to order

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		//initial state
 		this.state = {
 			fishes: {},
@@ -33,6 +34,15 @@ class App extends React.Component {
 		});
 	}
 
+	addToOrder(key) {
+		//take a copy of our state
+		const order = {...this.state.order}
+		//update or add the new number of fish ordered
+		order[key] = order[key] + 1 || 1;
+		//update out state
+		this.setState({ order });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -40,7 +50,7 @@ class App extends React.Component {
 					<Header cool={true} age="5000" tagline="Fresh Seafood Market"/>
 					<ul className="list-of-fishes">
 						{
-							Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+							Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} index={key}/>)
 						}
 					</ul>
 				</div>
