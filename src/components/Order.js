e52d08378bf542df9361acf3a6e23bf987375204
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2018-02-19 22:38:21 -0800

    displaying fish names and their individual prices

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,8 +2,27 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
+	constructor() {
+		super();
+		this.renderOrder = this.renderOrder.bind(this);
+	}
+	renderOrder(key) {
+		const fish = this.props.fishes[key];
+		const count = this.props.order[key];
+
+		if(!fish || fish.status === 'unavailable') {
+			return <li key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available!</li>
+		}
+
+		return (
+			<li key={key}>
+				<span>{count}lbs {fish.name}</span>
+				<span className="price">{formatPrice(count * fish.price)}</span>
+			</li>
+		)
+	}
 	render() {
-		const orderIds = Object.keys(this.props.order);
+		const orderIds = Object.keys(this.props.order);   // array of order id's
 		const total = orderIds.reduce((prevTotal, key) => {
 			const fish = this.props.fishes[key];
 			const count = this.props.order[key];
@@ -18,6 +37,7 @@ class Order extends React.Component {
 			<div className="order-wrap">
 				<h2>Your Order</h2>
 				<ul className="order">
+					{orderIds.map(this.renderOrder)}
 					<li className="total">
 						<strong>Total:</strong>
 						{formatPrice(total)}
