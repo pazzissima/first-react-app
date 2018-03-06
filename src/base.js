9a13cee6104f9c4c9168de71ca0d4868c368bb1e
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2018-03-05 17:12:14 -0800

    synced state with Firebase!

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyAvBlzhWuYB_rL0Duy-maLHTwMBWH6N4fE",
+    authDomain: "catch-of-the-day-s-nagaslaeva.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-s-nagaslaeva.firebaseio.com"
+});
+
+export default base;
