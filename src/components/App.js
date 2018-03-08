2cda8489181a3f80ca993d6fd7ebbe7d9a4d6218
Sofya Nagaslaeva <sofyanagaslaeva@gmail.com>
2018-03-07 21:59:10 -0800

    created method removeFish and passed it down to Inventory

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
 		//initial state
@@ -63,6 +64,12 @@ class App extends React.Component {
 		this.setState({ fishes })
 	}
 
+	removeFish(key) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null;
+		this.setState({ fishes })
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -92,6 +99,7 @@ class App extends React.Component {
 				<Order fishes={this.state.fishes} order={this.state.order} params={this.props.params}/>
 				<Inventory 
 					addFish={this.addFish} 
+					removeFish={this.removeFish} 
 					loadSamples={this.loadSamples} 
 					fishes={this.state.fishes}
 					updateFish={this.updateFish}
